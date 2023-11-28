911bacc67b6c68c9b9ef5e3c74f7469559741ff8
Klimova Natalia <skorpnat@gmail.com>
2023-11-28 13:13:47 +0600

    add check ZIP files test

---
+++ src/test/java/tests/CheckFilesFromArchive.java
@@ -0,0 +1,75 @@
+package tests;
+
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
+import com.opencsv.CSVReader;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.fail;
+
+public class CheckFilesFromArchive {
+    private ClassLoader cl = CheckFilesFromArchive.class.getClassLoader();
+
+    @DisplayName("Распаковка и проверка CVS из архива ZIP ")
+    @Test
+    void zipCvsFileTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("TestFiles.zip"))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".csv")) {
+                    CSVReader csvReader = new CSVReader(new InputStreamReader(zis));
+                    List<String[]> csvContent = csvReader.readAll();
+                    Assertions.assertArrayEquals(new String[]{";Marksa;;(955) 656-5656;;Email;;"}, csvContent.get(1));
+                    return;
+                }
+            }
+            fail("Отсутствует в архиве csv файл");
+        }
+    }
+
+    @DisplayName("Распаковка и проверка PDF из архива ZIP ")
+    @Test
+    void zipPdfFileTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("TestFiles.zip"))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".pdf")) {
+                    PDF pdf = new PDF(zis);
+                    assertThat(pdf.text).contains("Item # Description Qty Unit Price Discount Price");
+                    return;
+                }
+            }
+            fail("Отсутствует в архиве pdf файл");
+        }
+    }
+
+    @DisplayName("Распаковка и проверка XLX из архива ZIP ")
+    @Test
+    void zipXlxFileTest() throws Exception {
+        try (ZipInputStream zis = new ZipInputStream(
+                cl.getResourceAsStream("TestFiles.zip"))) {
+            ZipEntry entry;
+            while ((entry = zis.getNextEntry()) != null) {
+                if (entry.getName().contains(".xlsx")) {
+                    XLS xls = new XLS(zis);
+                    Assertions.assertEquals(xls.excel.getSheetAt(0).getRow(0).getCell(1)
+                            .getStringCellValue(), "Test Company");
+
+                    return;
+                }
+            }
+            fail("Отсутствует в архиве xlx файл");
+        }
+    }
+
+}
